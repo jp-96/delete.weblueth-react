657bc808edb448b114ca2ccdce4875766a12c938
jp-96 <jp-96@outlook.jp>
2023-03-04 09:41:29 +0000

    build

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,54 @@
-import { MicrobitContextProvider } from './context/MicrobitContext';
+import { CustomEventCallback, DeviceEffector, MicrobitContextProvider, ServicesEffector, useMicrobitActor } from './context/MicrobitContext';
+import { BoundCallback } from './statemachine/Context';
+
+import { MicrobitDevice } from './context/MicroBitDevice';
+import { MicrobitServices } from './context/MicroBitServices';
+
+import { MicrobitAccelerometer } from './services/MicrobitAccelerometer';
+import { MicrobitButton } from './services/MicrobitButton';
+import { MicrobitDeviceInformation } from './services/MicrobitDeviceInformation';
+import { MicrobitDfuControl } from './services/MicrobitDfuControl';
+import { MicrobitEvent } from './services/MicrobitEvent';
+import { MicrobitIoPin } from './services/MicrobitIoPin';
+import { MicrobitLed } from './services/MicrobitLed';
+import { MicrobitMagnetometer } from './services/MicrobitMagnetometer';
+import { MicrobitTemperature } from './services/MicrobitTemperature';
+import { MicrobitUart } from './services/MicrobitUart';
+
+export {
+    CustomEventCallback, DeviceEffector, ServicesEffector, useMicrobitActor,
+    BoundCallback,
+
+    MicrobitDevice,
+    MicrobitServices,
+
+    MicrobitAccelerometer,
+    MicrobitButton,
+    MicrobitDeviceInformation,
+    MicrobitDfuControl,
+    MicrobitEvent,
+    MicrobitIoPin,
+    MicrobitLed,
+    MicrobitMagnetometer,
+    MicrobitTemperature,
+    MicrobitUart
+}
 export default MicrobitContextProvider;
 
-export { CustomEventCallback, DeviceEffector, ServicesEffector, useMicrobitActor } from './context/MicrobitContext';
-export { DeviceEffector, ServicesEffector, useMicrobitActor } from './context/MicrobitContext';
-export { BoundCallback } from './statemachine/Context';
 
-export { MicrobitDevice } from './context/MicroBitDevice';
-export { MicrobitServices } from './context/MicroBitServices';
+// export { CustomEventCallback, DeviceEffector, ServicesEffector, useMicrobitActor } from './context/MicrobitContext';
+// export { BoundCallback } from './statemachine/Context';
 
-export { MicrobitAccelerometer } from './services/MicrobitAccelerometer';
-export { MicrobitButton } from './services/MicrobitButton';
-export { MicrobitDeviceInformation } from './services/MicrobitDeviceInformation';
-export { MicrobitDfuControl } from './services/MicrobitDfuControl';
-export { MicrobitEvent } from './services/MicrobitEvent';
-export { MicrobitIoPin } from './services/MicrobitIoPin';
-export { MicrobitLed } from './services/MicrobitLed';
-export { MicrobitMagnetometer } from './services/MicrobitMagnetometer';
-export { MicrobitTemperature } from './services/MicrobitTemperature';
-export { MicrobitUart } from './services/MicrobitUart';
+// export { MicrobitDevice } from './context/MicroBitDevice';
+// export { MicrobitServices } from './context/MicroBitServices';
+
+// export { MicrobitAccelerometer } from './services/MicrobitAccelerometer';
+// export { MicrobitButton } from './services/MicrobitButton';
+// export { MicrobitDeviceInformation } from './services/MicrobitDeviceInformation';
+// export { MicrobitDfuControl } from './services/MicrobitDfuControl';
+// export { MicrobitEvent } from './services/MicrobitEvent';
+// export { MicrobitIoPin } from './services/MicrobitIoPin';
+// export { MicrobitLed } from './services/MicrobitLed';
+// export { MicrobitMagnetometer } from './services/MicrobitMagnetometer';
+// export { MicrobitTemperature } from './services/MicrobitTemperature';
+// export { MicrobitUart } from './services/MicrobitUart';
